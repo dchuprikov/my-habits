3b09fb367418d32f4e4c5b3a72ca984daf7821c8
dchuprikov <dvchuprikov@gmail.com>
2026-08-17 13:45:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'my-habits-v3';
+const CACHE = 'my-habits-v4';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
